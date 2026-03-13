234c8bb7785a057026bb15987731af9815225067
Kirito-code1 <yigitalievshahzod821@gmail.com>
2026-03-13 21:19:37 +0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <>
+      <div className="w-full h-screen flex justify-center items-center">
+        <h1 className="text-3xl font-bold  ">Начало нашего проекта!</h1>
+      </div>
+    </>
+  );
+}
