158d6e044b8caf3984af791049aa944703ab830f
orentov122 <dreamernothdme@gmail.com>
2026-03-13 19:58:44 +0300

    Text brown

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ export default function App() {
   return (
     <>
       <div className="w-full h-screen flex justify-center items-center">
-        <h1 className="text-3xl font-bold  ">Начало нашего проекта!</h1>
+        <h1 className="text-3xl font-bold text-brown-500  ">Начало нашего проекта!</h1>
       </div>
     </>
   );
